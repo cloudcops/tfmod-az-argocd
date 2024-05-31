1a2d548a9e73bf3232a456b393d39ffb05e9a604
tf-automation <tf-automation@myperfectstay.ai>
2024-05-31 08:51:44 +0200

    Update versions.tf (Terraform Managed)

--- versions.tf
+++ versions.tf
@@ -1,25 +1,11 @@
+# DO NOT EDIT; MANAGED BY TERRAFORM
+# create override.tf file if you want to override specific values or add providers
 terraform {
   required_version = ">= 1.8.0"
   required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "2.47.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.97.1"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "1.14.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.29.0"
-    }
-    external = {
-      source  = "hashicorp/external"
-      version = "2.3.3"
+      version = "3.105.0"
     }
   }
 }
