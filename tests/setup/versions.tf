327b2ed40bb9b3e27976e203406cb86d97de3293
tf-automation <tf-automation@myperfectstay.ai>
2024-06-08 15:54:05 +0200

    chore(managed files): Update tests/setup/versions.tf

--- tests/setup/versions.tf
+++ tests/setup/versions.tf
@@ -9,3 +9,10 @@ terraform {
     }
   }
 }
+
+provider "azurerm" {
+  subscription_id            = "90d6584a-70ce-4e7f-835f-b9a8beee820f"
+  tenant_id                  = "501655c0-c0b2-4265-bf84-aa957f7272cf"
+  skip_provider_registration = true
+  features {}
+}
